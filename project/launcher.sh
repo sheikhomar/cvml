116e4c3e368e60b0778e1e0b8110497e3574744c
Omar Ali Sheikh-Omar <sheikhomar@gmail.com>
2018-04-08 11:47:54 +0200

    Script to submit jobs to Colifax Cluster

---
+++ project/launcher.sh
@@ -0,0 +1,13 @@
+#PBS -l walltime=24:00:00 -N cvml
+cd $PBS_O_WORKDIR
+
+if [ -z "$1" ]
+  then
+    echo "No script supplied: qsub launcher.sh -F 'myscript.py'"
+    exit 1
+fi
+
+echo Starting $1
+python $1
+echo Script $1 ended
+
