cd27a054f26f58df3e26c593e0c4505aa5a2c000
Omar Ali Sheikh <sheikhomar@gmail.com>
2018-04-09 21:51:43 -0700

    Fix runtime error.

--- project/model-02-vgg16-part-04.py
+++ project/model-02-vgg16-part-04.py
@@ -7,7 +7,7 @@ import h5py
 from keras import applications
 from keras.preprocessing.image import ImageDataGenerator
 from keras import optimizers
-from keras.models import Sequential, Model
+from keras.models import Sequential, Model, load_model
 from keras.layers import Dropout, Flatten, Dense, Conv2D
 from keras import backend as k
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler, TensorBoard, EarlyStopping
