a7ab9398687cd3036a85823649e4c19dfd824c98
Omar Ali Sheikh <oas@enversion.dk>
2018-04-15 20:00:10 +0200

    Generate new prediction.

--- project/predictor-vgg19.py
+++ project/predictor-vgg19.py
@@ -1,7 +1,8 @@
 from model_vgg19 import ModelVGG19
 
 model = ModelVGG19()
-path = 'saved_weights/model-04-vgg19-type-4/model-04-vgg19-type-4-epoch72-acc0.90-loss0.29-valacc0.84-valloss0.53.hdf5'
+# path = 'saved_weights/model-04-vgg19-type-4/model-04-vgg19-type-4-epoch72-acc0.90-loss0.29-valacc0.84-valloss0.53.hdf5'
+path = 'saved_weights/model-04-vgg19-type-4/model-04-vgg19-type-4-epoch56-acc0.85-loss0.44-valacc0.82-valloss0.58.hdf5'
 y_test_predictions = model.predict_test(model_weights=path)
 
-model.write_predictions(y_test_predictions, 'test-pred-model-04-vgg19-type-4-epoch72.csv')
+model.write_predictions(y_test_predictions, 'test-pred-model-04-vgg19-type-4-epoch56.csv')
