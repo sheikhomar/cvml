6dab1d999fd60c0ded5053c1cc8b904de8861210
Omar Ali Sheikh <oas@enversion.dk>
2018-04-15 14:20:57 +0200

    Rename files.

--- project/model-04-vgg19-type-5.py
+++ project/model-04-vgg19-type-5.py
@@ -1,3 +1,3 @@
 from model_vgg19 import ModelVGG19
 
-ModelVGG19().run(learning_rate=0.00001, n_frozen_layers=17, batch_size=128)
+ModelVGG19(learning_rate=0.00001, n_freeze_layers=17, batch_size=64).train()
