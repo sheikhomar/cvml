f05f9339cb22e7da2dc58947358306c67f44ddf7
Omar Ali Sheikh <sheikhomar@gmail.com>
2018-04-15 11:01:12 -0700

    Reduce batch size to avoid failure.

--- project/model-03-inceptionv3-part-01.py
+++ project/model-03-inceptionv3-part-01.py
@@ -1,3 +1,3 @@
 from model_inception_v3 import ModelInceptionV3
 
-ModelInceptionV3(learning_rate=0.00001, n_freeze_layers=0, batch_size=64).train()
+ModelInceptionV3(learning_rate=0.00001, n_freeze_layers=0, batch_size=8).train()
