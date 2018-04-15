76596e7024412d3590e3f0916054f6a83b99d8f5
Omar Ali Sheikh <oas@enversion.dk>
2018-04-15 19:28:38 +0200

    Script for making predictions.

--- project/predictor-vgg19.py
+++ project/predictor-vgg19.py
@@ -1,4 +1,7 @@
 from model_vgg19 import ModelVGG19
 
-model = ModelVGG19(model_name='model-04-vgg19-type-4')
-model.predict()
+model = ModelVGG19()
+path = 'saved_weights/model-04-vgg19-type-4/model-04-vgg19-type-4-epoch72-acc0.90-loss0.29-valacc0.84-valloss0.53.hdf5'
+y_test_predictions = model.predict_test(model_weights=path)
+
+model.write_predictions(y_test_predictions, 'test-pred-model-04-vgg19-type-4-epoch72.csv')
