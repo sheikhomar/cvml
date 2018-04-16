728a5a38d9534a3282c5582db56a4a920a6860dd
Omar Ali Sheikh <oas@enversion.dk>
2018-04-16 20:23:01 +0200

    Inception v3 based model.

---
+++ project/model-07-inceptionv3-take-2-part-02.py
@@ -0,0 +1,8 @@
+from model_inception_v3_take_2 import ModelInceptionV3Take2
+
+ModelInceptionV3Take2(
+    learning_rate=0.0000001,
+    n_freeze_layers=0,
+    batch_size=65,
+    verbose=1
+).train()
