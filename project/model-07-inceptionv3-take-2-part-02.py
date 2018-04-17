848a90d83138b29fd7f48e03042a5295073dbd19
Omar Ali Sheikh-Omar <sheikhomar@gmail.com>
2018-04-17 17:37:38 +0200

    Update stuff

--- project/model-07-inceptionv3-take-2-part-02.py
+++ project/model-07-inceptionv3-take-2-part-02.py
@@ -1,7 +1,7 @@
 from model_inception_v3_take_2 import ModelInceptionV3Take2
 
 ModelInceptionV3Take2(
-    learning_rate=0.0000001,
+    learning_rate=0.000001,
     n_freeze_layers=0,
     batch_size=2,
     verbose=1
