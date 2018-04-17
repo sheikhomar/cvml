1ecb44283de75670aaa6b866eccbd83de70711f4
Omar Ali Sheikh-Omar <sheikhomar@gmail.com>
2018-04-17 05:49:34 +0200

    Try Inception v3 model on GPU.

--- project/model-07-inceptionv3-take-2-part-02.py
+++ project/model-07-inceptionv3-take-2-part-02.py
@@ -3,6 +3,6 @@ from model_inception_v3_take_2 import ModelInceptionV3Take2
 ModelInceptionV3Take2(
     learning_rate=0.0000001,
     n_freeze_layers=0,
-    batch_size=65,
+    batch_size=2,
     verbose=1
 ).train()
