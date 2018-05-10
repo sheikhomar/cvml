05c527b3c76f6fc35b58fdff5c5fc455849a8593
Omar Ali Sheikh <sheikhomar@gmail.com>
2018-05-10 17:53:24 +0200

    kNN v2

---
+++ project/scripts/knn2.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from simple_model_base import SimpleModelBase
+
+
+class KNN(SimpleModelBase):
+  def __init__(self, *args, **kwargs):
+    SimpleModelBase.__init__(self, *args, **kwargs)
+
+  def _get_classifier(self):
+    # Estimate a covariance matrix
+    covariance_matrix = np.cov(self._get_train_data())
+
+    # Create a pipeline
+    return Pipeline([
+      ('scaler', StandardScaler()),
+      ('knn',
+        KNeighborsClassifier(
+          metric='mahalanobis',
+          metric_params={'V': covariance_matrix}
+        )
+      )
+    ])
+
+  def _get_search_grid_params(self):
+    return {
+      'knn__n_neighbors': np.arange(3, 29)
+    }
+
+
+if __name__ == '__main__':
+  KNN(n_jobs=-1).run()
