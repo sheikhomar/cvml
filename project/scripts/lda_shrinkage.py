8feea8b4a6a04fe1b80c4c8abc24f565b27abc18
Omar Ali Sheikh <sheikhomar@gmail.com>
2018-05-12 14:06:28 +0200

    Two LDA based models.

---
+++ project/scripts/lda_shrinkage.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from simple_model_base import SimpleModelBase
+
+
+class LDA(SimpleModelBase):
+  def __init__(self, *args, **kwargs):
+    SimpleModelBase.__init__(self, *args, **kwargs)
+
+  def _get_classifier(self):
+    return LinearDiscriminantAnalysis(solver='lsqr')
+
+  def _get_search_grid_params(self):
+    return {
+      "shrinkage": ['auto', 0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
+    }
+
+
+if __name__ == '__main__':
+  LDA().run()
