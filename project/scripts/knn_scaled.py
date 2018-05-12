0af64c6f5025066666464b32d6aba0da57869267
Omar Ali Sheikh <sheikhomar@gmail.com>
2018-05-12 12:42:12 +0200

    KNN with standard scaler.

---
+++ project/scripts/knn_scaled.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from simple_model_base import SimpleModelBase
+
+
+class KNN(SimpleModelBase):
+  def __init__(self, *args, **kwargs):
+    SimpleModelBase.__init__(self, *args, **kwargs)
+
+  def _get_classifier(self):
+    return Pipeline([
+      ('scaler', StandardScaler()),
+      ('knn', KNeighborsClassifier())
+    ])
+
+  def _get_search_grid_params(self):
+    return {
+      'knn__n_neighbors': np.arange(2, 29),
+      'knn__metric': ['manhattan', 'euclidean']
+    }
+
+
+if __name__ == '__main__':
+  KNN(n_jobs=-1).predict(classifier_params={
+      'n_neighbors': 16,
+      'metric': 'manhattan'
+    })
