525b83ba44072ad70e0f858ad63e9c48cf2cb0c9
Omar Ali Sheikh <sheikhomar@gmail.com>
2018-05-12 12:27:19 +0200

    Scale data before SVM.

--- project/scripts/svm.py
+++ project/scripts/svm.py
@@ -1,6 +1,7 @@
 
 from sklearn.svm import SVC
-
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
 from simple_model_base import SimpleModelBase
 
 class SVM(SimpleModelBase):
@@ -8,19 +9,17 @@ class SVM(SimpleModelBase):
     SimpleModelBase.__init__(self, *args, **kwargs)
 
   def _get_classifier(self):
-    return SVC()
+    return Pipeline([
+      ('scaler', StandardScaler()),
+      ('svm', SVC(kernel='rbf'))
+    ])
 
   def _get_search_grid_params(self):
     return {
-      'C': [0.01, 0.1, 1, 10, 100, 1000],
-      'gamma': [1, 0.1, 0.01, 0.001, 0.0001],
-      'kernel': ['rbf']
+      'svm__C': [0.01, 0.1, 1, 10, 100, 1000],
+      'svm__gamma': [10, 1, 0.1, 0.01, 0.001, 0.0001]
     }
 
 
 if __name__ == '__main__':
-    SVM().predict(classifier_params={
-      'gamma': 0.0001,
-      'C': 1,
-      'kernel': 'rbf'
-    })
+    SVM().run()
