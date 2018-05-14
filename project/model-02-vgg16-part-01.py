bef97dccfbbbd18bcb1186e39fa122bb6a1e90c8
Omar Ali Sheikh <sheikhomar@gmail.com>
2018-05-14 05:48:32 +0200

    ...

--- project/model-02-vgg16-part-01.py
+++ project/model-02-vgg16-part-01.py
@@ -1,3 +1,10 @@
 from model_vgg16 import ModelVGG16
 
-ModelVGG16(learning_rate=0.00001, n_freeze_layers=0, batch_size=64).train()
+m = ModelVGG16(
+  learning_rate=0.00001,
+  n_freeze_layers=0,
+  batch_size=64
+)
+m.train()
+m.predict_test()
+m.predict_instance_test()
