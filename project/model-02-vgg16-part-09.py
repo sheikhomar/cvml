aa35a2ea75516a15153f1a351def5561106d6b41
Omar Ali Sheikh <sheikhomar@gmail.com>
2018-05-14 14:24:12 +0200

    Train VGG16 from scratch using Adam optimiser.

---
+++ project/model-02-vgg16-part-09.py
@@ -0,0 +1,32 @@
+from keras.optimizers import Adam
+
+from model_base import ModelBase
+
+from keras import applications
+from keras.models import Sequential
+from keras.layers import Dropout, Flatten, Dense
+
+
+class ModelVGG16(ModelBase):
+    def __init__(self, *args, **kwargs):
+        ModelBase.__init__(self, *args, **kwargs)
+
+    def _create(self):
+        base_model = applications.VGG16(
+            weights=None,
+            include_top=False,
+            input_shape=(self.img_width, self.img_height, self.img_channels)
+        )
+        self.model = Sequential(base_model.layers)
+        self.model.add(Flatten())
+        self.model.add(Dense(4096, activation='relu'))
+        self.model.add(Dense(4096, activation='relu'))
+        self.model.add(Dense(self.n_labels, activation='softmax'))
+
+
+ModelVGG16(
+  learning_rate=None,
+  optimizer=Adam(),
+  n_freeze_layers=0,
+  batch_size=64
+).train()
